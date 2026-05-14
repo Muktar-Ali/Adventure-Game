a20438abdab524b434deb2c0a0a43bc20c73943b
Muktar Ali <muktarali1004@gmail.com>
2026-05-14 16:33:42 -0400

    Updated backend and choreo folder

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -17,11 +17,11 @@ class Story_Generator:
     @classmethod
     # private method to get the llm instance
     def _get_llm(cls):
-       openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
        serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
 
-       if openai_api_key and serviceurl:
-            return ChatOpenAI(model="gpt-4o-mini", api_key=openai_api_key, base_url=serviceurl)
+
+       if serviceurl:
+            return ChatOpenAI(model="gpt-4o-mini", base_url=serviceurl)
        
        return ChatOpenAI(model="gpt-4o-mini")
 
